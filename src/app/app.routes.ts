be38e9de3d9f6b43c952a390ea07a121c730ca45
Amado <amadoabaca99@gmail.com>
2024-10-07 00:54:47 -0300

    formulario de inicio v0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'login-form', component: LoginFormComponent },
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/login']);
+  }
+}
