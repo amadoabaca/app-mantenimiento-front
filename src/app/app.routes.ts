a504f91b0630aad3bccf8f865a69ee6864106239
Amado <amadoabaca99@gmail.com>
2024-10-17 21:22:34 -0300

    interfaz registro component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistroComponent } from './components/registro/registro.component';
 import { OrdenTrabajoComponent } from './components/orden-trabajo/orden-trabajo.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'login-form', component: LoginFormComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: 'orden-trabajo', component: OrdenTrabajoComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
