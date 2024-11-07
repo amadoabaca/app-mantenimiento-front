bde7df2ced708babeca3468275fcc964a31eed7a
Amado <amadoabaca99@gmail.com>
2024-11-07 01:38:59 -0300

    historial

--- src/app/components/historial/historial.component.ts
+++ src/app/components/historial/historial.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-historial',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './historial.component.html',
   styleUrl: './historial.component.css'
 })
 export class HistorialComponent {
-
+constructor( private router: Router ){}
+  goBack() {
+    this.router.navigate(['/dashboard-admin']);
+  }
+  
 }
